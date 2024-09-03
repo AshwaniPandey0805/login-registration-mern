58bc74fdc6f011dd725879b9961243b7e30beb85
Ashwani Kumar Pandey <ashwanipandey679@gmail.com>
2024-09-03 23:40:50 +0530

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-slate-400 text-center' >Hello world</h1>
+        
+    </>
+  )
+}
+
+export default App
